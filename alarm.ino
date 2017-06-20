155e25b76ed7c09b6020dbeba8f933d5b57ca26b
Artem Batalov <bat.artem2002@ya.ru>
2017-06-20 13:10:55 +0700

    ReadDtmf()

--- alarm.ino
+++ alarm.ino
@@ -6,7 +6,10 @@
 SoftwareSerial gsm(7, 6); // RX, TX
 TMRpcm tmrpcm;
 
+const String phone = "9069595030";
+
 String input_string;
+
 
 void setup() {
   Serial.begin(9600);
@@ -23,35 +26,7 @@ void loop() {
   gsm.println("AT");            // иначе модем засыпает
 
   if (gsm.find("RING")) {       // если нашли RING
-    Serial.println("RING!");
-    if (gsm.find("9069595030")) {
-      gsm.println("AT+DDET=1");   // включаем DTMF
-      delay(10);
-      gsm.println("ATA");         // поднимаем трубку
-      delay(500);
-      tmrpcm.play("parol.wav");
-      while (gsm.available()) gsm.read();
-      input_string = "";
-      while (!gsm.find("NO ")) {
-        while (gsm.available()) {
-          char c = gsm.read();
-          if (c == '\n') {
-            if (StrInt(input_string) != 0) {
-              Serial.println(StrInt(input_string));
-            }
-            input_string = "";
-          } else {
-            input_string += c;
-          }
-        }
-      }
-      gsm.println("ATH0");         // на всякий случай сбросим вызов
-      Serial.println("OK!");
-    } else {
-      gsm.println("ATH0");
-      Serial.println("NUM.ERROR. OK!");
-    }
-
+    ReadDtmf();
   }
   //gsm.println("ATH0");
 }
@@ -63,5 +38,36 @@ int StrInt(String str) {
     if (outstr.toInt() != 0) break;
   }
   return outstr.toInt();
+}
+
+void ReadDtmf() {
+  Serial.println("RING!");
+  if (gsm.find(phone)) {
+    gsm.println("AT+DDET=1");   // включаем DTMF
+    delay(10);
+    gsm.println("ATA");         // поднимаем трубку
+    delay(500);
+    tmrpcm.play("parol.wav");
+    while (gsm.available()) gsm.read();
+    input_string = "";
+    while (!gsm.find("NO ")) {
+      while (gsm.available()) {
+        char c = gsm.read();
+        if (c == '\n') {
+          if (StrInt(input_string) != 0) {
+            Serial.println(StrInt(input_string));
+          }
+          input_string = "";
+        } else {
+          input_string += c;
+        }
+      }
+    }
+    gsm.println("ATH0");         // на всякий случай сбросим вызов
+    Serial.println("OK!");
+  } else {
+    gsm.println("ATH0");
+    Serial.println("NUM.ERROR. OK!");
+  }
 }
 
